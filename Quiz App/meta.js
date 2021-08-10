c1bd283ce7d6a7a59a0239586c7186ac5ad71665
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-10 14:48:07 +0530

    Working Fine Now YESSSS!!!: master

--- Quiz App/meta.js
+++ Quiz App/meta.js
@@ -1,54 +1,45 @@
 const quizData = [
   {
-    question: "Which crop is sown on the largest area in India?",
-    a: "Rice",
-    b: "Wheat",
-    c: "Sugarcane",
-    d: "Maize",
+    question: "What is the most used programming language in 2019?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "Who is the President of US?",
+    a: "Florin Pop",
+    b: "Donald Trump",
+    c: "Ivan Saldano",
+    d: "Mihai Andrei",
+    correct: "b",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
     correct: "a",
   },
   {
-    question: "Entomology is the science that studies?",
-    a: "Behaviour of human beings",
-    b: "Insects",
-    c: "The origin and history of technical and scientific terms",
-    d: "The formation of rocks",
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
     correct: "b",
-  },
-  {
-    question: 'Grand Central Terminal, Park Avenue, New York is the world"s?',
-    a: "Highest railway station",
-    b: "Longest railway station",
-    c: "Largest railway station",
-    d: "None of the above",
-    correct: "c",
-  },
-  {
-    question: "Galileo was an astronomer who _______",
-    a: "Developed the telescope",
-    b: "Discovered four satellites of Jupiter",
-    c: "Discovered that the movement of pendulum produces a regular time measurement",
-    d: "All of the above",
-    correct: "b",
-  },
-  {
-    question:
-      "Corey Anderson who has hit the fastest ODI century in 36 balls is from?",
-    a: "England",
-    b: "Australia",
-    c: "West Indies",
-    d: "New Zealand",
-    correct: "d",
   },
 ];
 
 const quiz = document.getElementById("quiz");
 const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_ans");
-const b_text = document.getElementById("b_ans");
-const c_text = document.getElementById("c_ans");
-const d_text = document.getElementById("d_ans");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
